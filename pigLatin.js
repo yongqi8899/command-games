fa934c92ad8beffb93e276f839e8ef1091bbbe9f
Yongqi Zhang <zhangyongqi7@gmail.com>
2024-06-04 23:34:27 +0200

    add pigLatin V1

--- pigLatin.js
+++ pigLatin.js
@@ -1,1 +1,19 @@
-console.log("pigLatin");
+const english = process.argv.slice(2)
+let isVowel = "aeiouAEIOU".split("");
+let latinSatz = english.map((word)=>{
+  let latin;
+  if (isVowel.includes(word.charAt(0))) {
+    latin = word.concat("way");
+  }
+  if (!isVowel.includes(word.charAt(0)) && isVowel.includes(word.charAt(1))) {
+    latin = word.slice(1).concat(word[0], "ay");
+  }
+  if (
+    !isVowel.includes(word.charAt(0)) &&
+    !isVowel.includes(word.charAt(1))
+  ) {
+    latin = word.slice(2).concat(word.slice(0, 2), "ay");
+  }
+  return latin;
+})
+console.log(latinSatz.join(" "));
