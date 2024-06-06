16fbb34ac0ecb7d90eff6f563086dbeef1a3e0a5
Yongqi Zhang <zhangyongqi7@gmail.com>
2024-06-06 08:43:46 +0200

    add caesarCipher V1

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,1 +1,13 @@
-console.log("caesarCipher");
+let cipher = process.argv.slice(2)
+let alpha = 'abcdefghijklmnopqrstuvwxyz'.split("")
+let wordsArr = cipher[0].split(" ")
+let alphasArr = wordsArr.map((alphaArr)=>{
+    let newAlpha = []
+    alphaArr.split("").map(e => {
+        let index = alpha.indexOf(e)
+        let newIndex = index + Number(cipher[1]%26)
+        newAlpha.push(alpha[newIndex])
+    });
+    let newWord = newAlpha.join("");
+    console.log(newWord);
+})
