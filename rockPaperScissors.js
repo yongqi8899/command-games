b5cea9a6d1c69d31f0cb78c6530da4897c4a8d81
Yongqi Zhang <zhangyongqi7@gmail.com>
2024-06-07 12:06:12 +0200

    update compare

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,30 +1,34 @@
 class DetermineWinner {
-    #moves = ['rock', 'paper', 'scissors']
-    #message
-    constructor(message){
-        this.#message = message
+  #moves = ["rock", "paper", "scissors"];
+  #message;
+  constructor(message) {
+    this.#message = message;
+  }
+  computerMove() {
+    return this.#moves[Math.floor(Math.random() * this.#moves.length)];
+  }
+  playerMove() {
+    return process.argv[2].toLowerCase();
+  }
+  compare() {
+    const player = this.playerMove(); // Call playerMove() inside compare()
+    const computer = this.computerMove(); // Call computerMove() inside compare()
+
+    if (!this.#moves.includes(player)) {
+      this.#message = `Please move 'rock', 'paper' or 'scissors'`;
+    } else if (player === computer) {
+      this.#message = "draw";
+    } else if (
+      (player === "rock" && computer === "scissors") ||
+      (player === "paper" && computer === "rock") ||
+      (player === "scissors" && computer === "paper")
+    ) {
+      this.#message = "you are winner";
+    } else {
+      this.#message = `Better luck next time`;
     }
-    computerMove(){
-        return this.#moves[Math.floor(Math.random()*(this.#moves.length))]
-    }
-    playerMove(){
-        return process.argv[2].toLowerCase()
-    }
-    compare(player, computer){
-        if (!this.#moves.includes(player)){
-            this.#message = `Please move 'rock', 'paper' or 'scissors'`
-        } else if (player === computer){
-            this.#message = 'draw'
-        }
-        else if (player === 'rock' && computer === 'scissors' || player === 'paper' && computer === 'rock' || player === 'scissors' && computer === 'paper'){
-            this.#message = 'you are winner'
-        } else {
-            this.#message = `Better luck next time`
-        }
-        return this.#message
-    }
+    return this.#message;
+  }
 }
-const determineWinner = new DetermineWinner()
-let player = determineWinner.playerMove()
-let computer = determineWinner.computerMove()
-console.log(determineWinner.compare(player, computer))
+const determineWinner = new DetermineWinner();
+console.log(determineWinner.compare());
